5b59a820485351e513fb68c282f5553b3a8d4eb2
Dawid Jeż <dawidjez@gmail.com>
2023-11-01 21:29:11 +0100

    map relations and add api call

--- js/main.js
+++ js/main.js
@@ -1,55 +1,38 @@
 import {mapArtist, mapBook, mapCd} from "./mappers.js";
 
+function deactivateItems() {
+  document.querySelectorAll('.item').forEach(item => item.classList.remove('active'))
+}
+
 document.body.addEventListener('click', e => {
-  if (e.target.matches('.item')) {
-    e.target.classList.add('active');
+  const el = e.target;
+  if (el.matches('.item')) {
+    el.classList.add('active');
   }
-  if (e.target.matches('.close')) {
-    e.target.closest('.item').classList.remove('active');
+  if (el.matches('.related-item')) {
+    const {type, id} = el.dataset;
+    const foundItem = document.querySelector(`.item[data-type="${type}"][data-id="${id}"]`)
+    if (foundItem) {
+      deactivateItems()
+      foundItem.classList.add('active')
+    }
+  }
+  if (el.matches('.close')) {
+    el.closest('.item').classList.remove('active');
   }
 });
 
-
-fetch('./json/data.json').then(res => res.json())
+fetch('http://localhost:4000/api/all').then(res => res.json())
   .then(data => {
-    const {books, artists, albums} = data;
-
-    const booksList = books.map(book => {
-      book.type = 'book';
-      book.artists = artists.filter(artist => book.artistsIds.includes(artist.id));
-      return book;
-    });
-    const artistsList = artists.map(artist => {
-      artist.type = 'artist';
-      artist.albums = albums.filter(album => album.artistsIds.includes(artist.id));
-      artist.books = books.filter(book => book.artistsIds.includes(artist.id));
-      return artist;
-    });
-    const albumsList = albums.map(album => {
-      album.type = 'album';
-      album.artists = artists.filter(artist => album.artistsIds.includes(artist.id));
-      return album;
-    });
-
-    document.querySelector('.grid').innerHTML = [
-      ...booksList,
-      ...albumsList,
-      ...artistsList,
-    ].sort((a, b) => a.name.localeCompare(b.name))
-      .map(item => {
-        let val = '';
-        switch (item.type) {
-          case 'album':
-            val = mapCd(item);
-            break;
-          case 'artist':
-            val = mapArtist(item);
-            break;
-          case 'book':
-            val = mapBook(item);
-            break;
-        }
-        return val;
-      }).join('');
-
+    document.querySelector('.grid').innerHTML = data.map(el => {
+      if (el.type === 'album') {
+        return mapCd(el)
+      }
+      if (el.type === 'book') {
+        return mapBook(el)
+      }
+      if (el.type === 'artist') {
+        return mapArtist(el)
+      }
+    }).join('');
   });
--- js/mappers.js
+++ js/mappers.js
@@ -3,8 +3,9 @@ function getItemArtists(item) {
 }
 
 function mapItem(item, content) {
-  return `<div class="item ${item.type}">
-           <img src="./svg/${item.type}.svg" alt="">
+  const {type, id, cover} = item;
+  return `<div class="item ${type}" data-type="${type}" data-id="${id}">
+           <img src="${cover || './svg/' + type + '.svg'}" alt="">
             ${content}
             <div class="close">
                 <img src="./svg/close.svg" alt="">
@@ -12,23 +13,44 @@ function mapItem(item, content) {
           </div>`;
 }
 
+function mapRelatedItem(type) {
+  return (item) => {
+    const {id, cover} = item;
+    return `<div class="related-item ${type}" data-type="${type}" data-id="${id}">
+              <img src="${cover || './svg/' + type + '.svg'}" alt="">
+              <p>${item.name}</p>
+          </div>`
+  };
+}
+
 export function mapCd(item) {
   return mapItem(item, `
       <h2>${item.name}</h2>
-      <p>${getItemArtists(item)}</p>`
+      <p>${getItemArtists(item)}</p>
+      <div class="related">
+        ${item.artists.map(mapRelatedItem('artist')).join('')}
+      </div>`
   );
 }
 
 export function mapBook(item) {
   return mapItem(item, `
       <h2>${item.name}</h2>
-      <p>${getItemArtists(item)}</p>`
+      <p>${getItemArtists(item)}</p>
+      <div class="related">
+        ${item.artists.map(mapRelatedItem('artist')).join('')}
+      </div>`
   );
 }
 
 export function mapArtist(item) {
   return mapItem(item, `
-      <h2>${item.name}</h2>`
+      <h2>${item.name}</h2>
+      <div class="related">
+        ${item.albums.map(mapRelatedItem('album')).join('')}
+        ${item.books.map(mapRelatedItem('book')).join('')}
+      </div>
+`
   );
 }
 
